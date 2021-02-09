ef34985c5afabea52e4b13613d9655a2c8e8cb4a
sylvia9915 <77959889+sylvia9915@users.noreply.github.com>
2021-02-09 14:41:27 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-
+app.get("/json", (req, res) => { let message = "Hello json";
+(process.env.MESSAGE_STYLE == "uppercase") ? message=message.toUpperCase() : message=message; 
+ res.json({"message": message}); 
+ });
 
 
 
